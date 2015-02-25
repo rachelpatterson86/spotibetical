27734aa211032af554dce710d1ed3168362b9a4e
Nick Ray <nickray22@yahoo.com>
2015-02-25 16:35:53 -0500

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20150225205952) do
+
   create_table "playlists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -24,6 +26,8 @@
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "songs", ["spotify_id"], name: "index_songs_on_spotify_id", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -42,3 +46,5 @@
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+end
