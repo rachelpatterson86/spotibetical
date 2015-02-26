949a2f68389e8e30ad1c55eb5bafdfe83a8b1dad
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-02-26 11:29:08 -0500

    search list shows on view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,14 @@ class UsersController < ApplicationController
     if params[:q]
       @query = RSpotify::Track.search(params[:q], limit: 20)
     end
+    # respond_to do |format|
+    #   format.html { render :search }
+    #   format.js { render :results }
   end
-  
+
 end
+
+# respond_to for HARD MODE. code in search method.
 
 #play song, ex: https://open.spotify.com/track/61fVrohjzviG6WWoo8Ihnl
 #@query.first.external_urls["spotify"]
