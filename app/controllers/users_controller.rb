9ba339534dd1568992c3e56ea005071c9be57c5b
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-02-26 00:35:01 -0500

    need search page to render results

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
 
   def search
     if params[:q]
-      query = RSpotify::Track.search(params[:q])
+      @query = RSpotify::Track.search(params[:q], limit: 20)
     end
   end
-
+  
 end
 
 #play song, ex: https://open.spotify.com/track/61fVrohjzviG6WWoo8Ihnl
