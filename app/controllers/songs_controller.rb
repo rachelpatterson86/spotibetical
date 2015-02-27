b55da22e08b9de934c2ef2c368720b43e9945a57
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-02-27 02:15:40 -0500

    display song list and sudo erb code for view

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,28 @@
 class SongsController < ApplicationController
+
+  def show
+    @songs = Song.all
+  end
+
 end
+
+# sudo erb 
+# <table>
+#   <tr>
+#     <td>Rank</td>
+#     <td>Artist</td>
+#     <td>Song</td>
+#     <td>Up Vote</td>
+#     <td>Down Vote</td>
+#   </tr>
+#   <tr>
+#     <%= <td>s.blahblahblahBANG</td> %>
+#     <% @songs.each do |s| %>
+  #     <%= <td>s.artist</td> %>
+  #     <%= <td>s.title</td> %>
+  #     <%= link_to("Up Vote",songs_show_path, method: :post) %>
+  #     <%= link_to("Down Vote",songs_delete_path, method: :destroy) %>
+#     <% end %>
+#   </tr>
+# </table>
+#
