d17df59ee30e26d2b0725ff4d3402340db9dd03b
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-02-27 14:29:57 -0500

    add user votes col

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227063828) do
+ActiveRecord::Schema.define(version: 20150227183706) do
 
   create_table "playlists", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150227063828) do
     t.integer  "song_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_vote"
   end
 
 end
