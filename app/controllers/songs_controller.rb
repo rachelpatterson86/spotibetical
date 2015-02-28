f520ef59932e70e4752b15fbe888e83a8810809b
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-02-28 00:13:35 -0500

    refactored saving votes to db

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,8 +2,7 @@ class SongsController < ApplicationController
 
   def submit
     @song = Song.create(song_params)
-    @song.votes.create(:user_id => current_user.id,
-                       :user_vote => 1)
+    upvote(@song)
     #@song.users = [current_user] better if there are no additional attributes
     render :show
   end
@@ -12,16 +11,10 @@ class SongsController < ApplicationController
     @songs = Song.all
   end
 
-  def upvote
-    @song = Song.find(:id).vote
-    #@song.users << current_user
-    @song.votes.create(:user_id => current_user.id,
-    :user_vote => 1)
-  end
-
-  def downvote
-    @song.votes.create(:user_id => current_user.id,
-    :user_vote => -1)
+  def vote
+    @song = Song.find(:id)
+    upvote(@song)
+    downvote(@song)
   end
 
 private
@@ -29,24 +22,14 @@ private
     def song_params
       params.require(:song).permit(:user_id, :artist, :title, :spotify_id)
     end
-end
 
-# sudo erb
-# <table>
-#   <tr>
-#     <td>Rank</td>
-#     <td>Artist</td>
-#     <td>Song</td>
-#     <td>Up Vote</td>
-#     <td>Down Vote</td>
-#   </tr>
-#   <tr>
-#     <%= <td>s.blahblahblahBANG</td> %>
-#     <% @songs.each do |s| %>
-  #     <%= <td>s.artist</td> %>
-  #     <%= <td>s.title</td> %>
-  #     <%= link_to("Up Vote",songs__path, method: :post) %>
-  #     <%= link_to("Down Vote",songs_delete_path, method: :post) %>
-#     <% end %>
-#   </tr>
-# </table>
+    def upvote(song)
+      song.votes.create(:user_id => current_user.id,
+      :user_vote => 1)
+    end
+
+    def downvote(song)
+      song.votes.create(:user_id => current_user.id,
+      :user_vote => -1)
+    end
+end
