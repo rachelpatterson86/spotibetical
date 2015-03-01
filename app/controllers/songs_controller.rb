a848550d243c8635fd1fa9d93362f1dbd2dfbf0e
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-03-01 13:48:06 -0500

    integrate veto, improved vote

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,7 @@ class SongsController < ApplicationController
     @song = Song.create(song_params)
     upvote(@song)
     #@song.users = [current_user] better if there are no additional attributes
+    vote_limit
     render :show
   end
 
@@ -13,8 +14,25 @@ class SongsController < ApplicationController
 
   def vote
     @song = Song.find(:id)
-    upvote(@song)
-    downvote(@song)
+    if current_user.find_by(veto: true)
+      flash[:alert] = "You've used your veto this week. Should have thought about it more wisely."
+    elsif @song.votes.find_by(user_vote: 0)
+      flash[:notice] = "Song has been veto and cannot be voted until next week."
+    elsif upvote(@song) || downvote(@song)
+      vote_limit
+    end
+  end
+
+  def veto
+    @song = Song.find(:id)
+    if current_user.veto == true
+      flash[:alert] = "You've used your veto this week. Should have thought about it more wisely."
+    else
+      @song.votes.create(:user_id => current_user.id, user_vote: 0)
+      current_user@user.update(veto: true)
+      #User.find(current_user.id).update(veto: 0)
+      flash[:notice] = "You've used your single veto for this week."
+    end
   end
 
 private
@@ -31,5 +49,11 @@ private
     def downvote(song)
       song.votes.create(:user_id => current_user.id,
       :user_vote => -1)
+    end
+
+    def vote_limit
+      if Vote.vote_counter(current_user)
+        flash[:alert] = "You've used all 15 votes this week."
+      end
     end
 end
