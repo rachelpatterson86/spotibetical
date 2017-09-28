d65424120f3029f0832c632858be669dde888043
Rachel Patterson <rachel.patterson.1986@gmail.com>
2017-09-27 22:16:30 -0400

    Disable button if a song has already been suggested and add note that the song was added.

--- app/services/spotify_service.rb
+++ app/services/spotify_service.rb
@@ -2,12 +2,21 @@ class SpotifyService
   QUERY_LIMIT = 20
 
   def self.search(query)
-    spotify_track.search(query, limit: QUERY_LIMIT)
+    begin
+      spotify_track.search(query, limit: QUERY_LIMIT).each do |track|
+        track.instance_variable_set(:@submitted, submitted?(track.id))
+      end
+    rescue StandardError => e
+    end
   end
 
   private
 
   def self.spotify_track
     RSpotify::Track
+  end
+
+  def self.submitted?(id)
+    Song.find_by_spotify_id(id)&.votes&.recent_date_range ? true : false
   end
 end
--- spec/services/spotify_service_spec.rb
+++ spec/services/spotify_service_spec.rb
@@ -3,13 +3,57 @@ require 'rails_helper'
 RSpec.describe SpotifyService do
   let(:query) { 'red' }
 
-  before { allow(RSpotify::Track).to receive(:search) }
+  let(:spotify_result) do
+    [ SpotifyResult.new(12345), SpotifyResult.new(343242) ]
+  end
+
+  let(:search_result) do
+    spotify_result[0].submitted = false
+    spotify_result[1].submitted = true
+
+    spotify_result
+  end
+
+  subject { described_class.search(query) }
 
   describe '.search' do
-    it "calls spotify api" do
-      expect(RSpotify::Track).to receive(:search).with(query, limit: 20)
+    context 'when there is a query' do
+      before do
+        allow(RSpotify::Track).to receive(:search).and_return(spotify_result)
+        user = User.new
+        song = Song.create(spotify_id: spotify_result[1].id, user: user)
+        Vote.create(song: song, score: 1, user: user)
 
-      SpotifyService.search(query)
+        subject
+      end
+
+      it { is_expected.to eq(search_result) }
     end
+
+    context 'when query is blank' do
+      let(:query) { '' }
+
+      before do
+        allow(RSpotify::Track)
+          .to receive(:search)
+            .and_raise(StandardError.new('I AM AN ERROR'))
+      end
+
+      it 'does not return an error' do
+        expect{subject}.not_to raise_error
+      end
+
+      it { is_expected.to be_nil }
+    end
+  end
+end
+
+class SpotifyResult
+  attr_reader :id
+  attr_accessor :submitted
+  
+  def initialize(id, submitted=nil)
+    @id = id
+    @submitted = submitted
   end
 end
